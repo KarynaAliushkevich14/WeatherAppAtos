0b02e8468093b2b15624dfdb1c18c91f9c1052fd
Karyna Aliushkevich <89156053+KarynaAliushkevich14@users.noreply.github.com>
2022-09-18 12:02:16 +0200

    Implement cityWeather functionality

--- src/main/java/myweather/controller/WeatherController.java
+++ src/main/java/myweather/controller/WeatherController.java
@@ -1,4 +1,5 @@
 package myweather.controller;
+
 import lombok.AllArgsConstructor;
 import myweather.model.CityWithSelection;
 import myweather.model.Weather;
@@ -7,74 +8,102 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
-@AllArgsConstructor
+//@AllArgsConstructor
 public class WeatherController {
 
-    private final CityService cityService;
+    private static final Logger LOGGER = Logger.getLogger(WeatherController.class.getName());
 
-    ArrayList<CityWithSelection> myCityOrder ;
+    private CityService cityService;
+
+    public WeatherController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    //List<CityWithSelection> myCityOrder;
 
     @GetMapping("/cityList")
     public String index(ModelMap model) {
-        myCityOrder = (ArrayList)cityService.getCitiesIndex();
+        List<CityWithSelection> myCityOrder = cityService.getCitiesIndex();
         model.addAttribute("cities", myCityOrder);
         return "index";
     }
 
     @PostMapping("/selectedCities")
-    public String postListOfCities (@ModelAttribute ArrayList<CityWithSelection> myCityOrder)  {
-        this.myCityOrder=myCityOrder;
-        System.out.println("works");
-        return "redirect:/selectedCities";
+    public String postListOfCities(Integer[] cityIndexes, ModelMap modelMap) {
+        LOGGER.info("postListOfCities(" + Arrays.toString(cityIndexes) + ")");
+        //this.myCityOrder = myCityOrder;
+        if (cityIndexes != null) {
+            List<CityWithSelection> cityWithSelections = new ArrayList<>();
+            for (Integer cityIndex : cityIndexes) {
+                CityWithSelection cityWithSelection = cityService.getById(cityIndex);
+                cityWithSelections.add(cityWithSelection);
+            }
+            //modelMap.addAttribute("cityWithSelections", cityWithSelections);
+            LOGGER.info("cityWithSelections: " + cityWithSelections);
+
+            List<Weather> weather = new ArrayList<>();
+            for (CityWithSelection cityWithSelection : cityWithSelections) {
+                weather.add(cityService.getWeather(cityWithSelection.getLat(), cityWithSelection.getLon()));
+            }
+            LOGGER.info("weather: " + weather);
+            modelMap.addAttribute("weatherFromSelectedCities", weather);
+        }
+
+        //return "redirect:/selectedCities";
+        return "selectedCities";
     }
 
     @GetMapping(path = "/selectedCities")
-    public String getSelectedCities(ModelMap modelMap){
-        List<Weather> allCitiesWeathers = weatherByLatLon(); // metoda do pobrania 10 weather z miast wyszukiwanych by Id. Napisana nizej tego geta
+    public String getSelectedCities(ModelMap modelMap) {
+        //List<Weather> allCitiesWeathers = weatherByLatLon(); // metoda do pobrania 10 weather z miast wyszukiwanych by Id. Napisana nizej tego geta
         List<Weather> weatherFromSelectedCities = new ArrayList<>();
+        List<CityWithSelection> cityWithSelections = (List<CityWithSelection>) modelMap.getAttribute("cityWithSelections");
+        LOGGER.info("cityWithSelections: " + cityWithSelections);
 
-        if (!(myCityOrder.get(0).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(0));
-        }
-        if (!(myCityOrder.get(1).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(1));
-        }
-        if (!(myCityOrder.get(2).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(2));
-        }
-        if (!(myCityOrder.get(3).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(3));
-        }
-        if (!(myCityOrder.get(4).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(4));
-        }
-        if (!(myCityOrder.get(5).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(5));
-        }
-        if (!(myCityOrder.get(6).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(6));
-        }
-        if (!(myCityOrder.get(7).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(7));
-        }
-        if (!(myCityOrder.get(8).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(8));
-        }
-        if (!(myCityOrder.get(8).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(8));
-        }
-        if (!(myCityOrder.get(9).getName() == null)){
-            weatherFromSelectedCities.add(allCitiesWeathers.get(9));
-        }
+//        if (!(myCityOrder.get(0).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(0));
+//        }
+//        if (!(myCityOrder.get(1).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(1));
+//        }
+//        if (!(myCityOrder.get(2).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(2));
+//        }
+//        if (!(myCityOrder.get(3).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(3));
+//        }
+//        if (!(myCityOrder.get(4).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(4));
+//        }
+//        if (!(myCityOrder.get(5).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(5));
+//        }
+//        if (!(myCityOrder.get(6).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(6));
+//        }
+//        if (!(myCityOrder.get(7).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(7));
+//        }
+//        if (!(myCityOrder.get(8).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(8));
+//        }
+//        if (!(myCityOrder.get(8).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(8));
+//        }
+//        if (!(myCityOrder.get(9).getName() == null)) {
+//            weatherFromSelectedCities.add(allCitiesWeathers.get(9));
+//        }
 
         modelMap.addAttribute("weatherFromSelectedCities", weatherFromSelectedCities);
 
         return "selectedCities";
-
     }
 
     //method that make new list of selected cities
--- src/main/java/myweather/model/CityWithSelection.java
+++ src/main/java/myweather/model/CityWithSelection.java
@@ -5,9 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-@Getter
-@Setter
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@NoArgsConstructor
 //@AllArgsConstructor
 public class CityWithSelection {
     private String id;
@@ -15,7 +15,9 @@ public class CityWithSelection {
     private String lat;
     private String lon;
 
-    
+    public CityWithSelection() {
+    }
+
     public CityWithSelection(String id, String name, String lat, String lon) {
     	this.id=id;
     	this.name=name;
@@ -23,8 +25,45 @@ public class CityWithSelection {
     	this.lon=lon;
     }
 
-    
-    
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    @Override
+    public String toString() {
+        return "CityWithSelection{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", lat='" + lat + '\'' +
+                ", lon='" + lon + '\'' +
+                '}';
+    }
 }
